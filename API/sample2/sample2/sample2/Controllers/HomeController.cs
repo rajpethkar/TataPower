be6fd571711f710468d7fdd90d95098efdd452fc
Chiranjibi Sahu <chiranjibi098@gmail.com>
2022-12-21 00:45:01 +0530

    API

---
+++ API/sample2/sample2/sample2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿//https://www.youtube.com/watch?v=TbL1UdfwWWI&ab_channel=PravinUgalmugale
+
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using sample2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+
+namespace sample2.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+
+
+            return Ok(new { EmpName = "Rajesh" });
+        }
+
+        //[HttpPost]
+        //public void AddEmp([FromBody] string value)
+        //{
+
+        //}
+
+        [HttpPost]
+        public IHttpActionResult AddEmp(Employee req)
+        {
+            var json = JsonConvert.SerializeObject(req);
+            return Ok(json);
+        }
+
+        [HttpPut]
+        public IHttpActionResult EditEmp(Employee req)
+        {
+            var json = JsonConvert.SerializeObject(req);
+            return Ok(json);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteEmp(int id)
+        {
+
+            return Ok();
+        }
+
+    }
+}
---
+++ API/sample2/sample2/sample2/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample2.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
